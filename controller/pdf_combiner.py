eac204c034fc82f4b71ebba7e097c9228cafc35f
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-16 15:51:37 -0600

    Add option to run module on it's own in controller\pdf_combiner.py

--- controller/pdf_combiner.py
+++ controller/pdf_combiner.py
@@ -2,9 +2,12 @@ from typing import List
 import PyPDF2
 
 
-def pdf_combiner(pdf_list: List[str], output_file: str = 'super.pdf') -> None:
+def pdf_combiner(pdf_list: List[str], output_file: str = 'out.pdf') -> None:
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         print(pdf)
         merger.append(pdf)
     merger.write(output_file)
+
+if __name__ == '__main__':
+    print('Executing from module: pdf_combiner.py')
