d2240f19fd71119df12c579b53053255942cddb2
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-17 00:48:07 -0600

    Add validation to controller\pdf_combiner.py

--- controller/pdf_combiner.py
+++ controller/pdf_combiner.py
@@ -1,13 +1,31 @@
 from typing import List
 import PyPDF2
+from pathlib import Path
 
 
-def pdf_combiner(pdf_list: List[str], output_file: str = 'out.pdf') -> None:
+def pdf_combiner(pdf_list: List[Path], output_file: Path = Path('out.pdf')) -> None:
+    if output_file.exists():
+        raise FileExistsError(
+            f"Can't create the {output_file} output file. It already exists.")
+
     merger = PyPDF2.PdfFileMerger()
     for pdf in pdf_list:
         print(pdf)
         merger.append(pdf)
+
     merger.write(output_file)
+    merger.close()
+
 
 if __name__ == '__main__':
-    print('Executing from module: pdf_combiner.py')
+    # Create a list of pdf files to merge.
+    path_to_pdf_files = Path(r'C:\Users\Jorge\git\pdf-merger')
+
+    output_file_path = path_to_pdf_files / 'merged.pdf'
+
+    list_of_pdf_files = list(path_to_pdf_files.glob('**/*.pdf'))
+
+    try:
+        pdf_combiner(pdf_list=list_of_pdf_files, output_file=output_file_path)
+    except FileExistsError as e:
+        print(e)
