e13168d19ba74b00835214fdfd58a367b95902b1
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-19 14:33:26 -0600

    Add type validation to input parameters to controller\pdf_combiner.py

--- controller/pdf_combiner.py
+++ controller/pdf_combiner.py
@@ -4,6 +4,26 @@ from pathlib import Path
 
 
 def pdf_combiner(pdf_list: List[Path], output_file: Path = Path('out.pdf')) -> None:
+    """pdf_combiner function gets a list of Paths (pathlib.Path) pointing at PDF files, then merges
+    them into a single PDF file of the name defined in output_file parameter or out.pdf if no
+    argument is passed to the output_file parameter.
+
+    Examples:
+    pdf_filepaths = [Path('file1.pdf'), Path('file2.pdf'), Path('file3.pdf')]
+    pdf_combiner(pdf_filepaths) # out.pdf is the merged PDF file.
+    pdf_combiner(pdf_filepaths, output_file=Path('merge.pdf'))
+    """
+    if not isinstance(pdf_list, List):
+        raise TypeError("Parameter pdf_list, is not of type List")
+
+    for index, path in enumerate(pdf_list):
+        if not isinstance(path, Path):
+            raise TypeError(
+                f"Element: {index} from List is not of type {Path}. Value: {path}")
+
+    if not isinstance(output_file, Path):
+        raise TypeError("Parameter output_file, is not of type Path")
+
     if output_file.exists():
         raise FileExistsError(
             f"Can't create the {output_file} output file. It already exists.")
@@ -13,8 +33,8 @@ def pdf_combiner(pdf_list: List[Path], output_file: Path = Path('out.pdf')) -> N
         print(pdf)
         merger.append(pdf)
 
-    merger.write(output_file)
-    merger.close()
+    # merger.write(output_file)
+    # merger.close()
 
 
 if __name__ == '__main__':
