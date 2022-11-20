fcc178370df11d099b39cd393d25fdc9884703c1
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-20 09:58:43 -0600

    Refactor controller\greet.py

--- controller/greet.py
+++ controller/greet.py
@@ -1,8 +1,15 @@
-import tkinter as tk
+"""2022-11-20 09:08:57"""
 
 
 def greet(user_name: str = None) -> None:
     """This functions greets you on the console. Send an argument to user_name parameter to set the
     name of the person to greet, or leave it empty to greet the world."""
 
+    if not isinstance(user_name, str) and user_name is not None:
+        raise TypeError("Parameter user_name, is not of type str (string)")
+
     print(f"Hello, {user_name or 'World!'}")
+
+
+if __name__ == '__main__':
+    greet()
