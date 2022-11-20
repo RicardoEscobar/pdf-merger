ccfd6add72cb14c910a38fbc575ca6d8255002b5
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-20 09:58:54 -0600

    Add test_greet.py

---
+++ test_greet.py
@@ -0,0 +1,15 @@
+import unittest
+from controller.greet import greet
+
+
+class TestGreet(unittest.TestCase):
+    """Unit tests for the greet funtion"""
+
+    def test_user_name_is_str(self):
+        """Test user_name is str or None"""
+        test_user_name_param = 1
+        self.assertRaises(TypeError, greet, test_user_name_param)
+
+
+if __name__ == '__main__':
+    unittest.main()
