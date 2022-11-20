37e700d3b1e9331ea5efd450b34e6eb6828f5c95
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-20 08:59:06 -0600

    Add docstring to controller\greet.py

--- controller/greet.py
+++ controller/greet.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 
+
 def greet(user_name: str = None) -> None:
-    # The get() method is used to fetch the value of a StringVar() instance.
-    # If user_name is empty, print Hello, World!
+    """This functions greets you on the console. Send an argument to user_name parameter to set the
+    name of the person to greet, or leave it empty to greet the world."""
+
     print(f"Hello, {user_name or 'World!'}")
