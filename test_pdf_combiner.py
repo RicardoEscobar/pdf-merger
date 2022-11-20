c4e8ca3ce355304b26644375f73de7e343ba1a02
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-20 11:03:33 -0600

    Refactor lists and tupples test_pdf_combiner.py

--- test_pdf_combiner.py
+++ test_pdf_combiner.py
@@ -7,20 +7,17 @@ from controller.pdf_combiner import pdf_combiner
 class TestPdfCombiner(unittest.TestCase):
     """Unit tests for the pdf_combiner funtion"""
 
+    def setUp(self) -> None:
+        self.list_of_pdf_files = list(Path('.').glob('**/*.pdf'))
+
     def test_pdf_list_param_is_a_list_of_paths(self):
         """Test that validates that the List of paths given to the pdf_combiner function is actually
         a list of pathlib.Paths objects"""
-        test_tuple_of_paths_param = (
-            Path('dummy.pdf'),
-            Path('twopage.pdf'),
-            Path('wtr.pdf')
-        )
+        test_tuple_of_paths_param = tuple(self.list_of_pdf_files)
 
-        test_list_of_paths_param = [
-            Path('dummy.pdf'),
-            'twopage.pdf',
-            Path('wtr.pdf')
-        ]
+        test_list_of_paths_param = list(
+            map(lambda file: file.name if file.name == 'twopage.pdf' else file,
+                self.list_of_pdf_files))
 
         self.assertRaises(TypeError, pdf_combiner, test_tuple_of_paths_param)
         self.assertRaises(TypeError, pdf_combiner, test_list_of_paths_param)
@@ -28,14 +25,9 @@ class TestPdfCombiner(unittest.TestCase):
     def test_output_file_param_is_a_path(self):
         """Test that validates that the output_file parameter is actually a pathlib.Path object."""
         test_output_file_param = 'merge.pdf'
-        test_list_of_paths_param = [
-            Path('dummy.pdf'),
-            Path('twopage.pdf'),
-            Path('wtr.pdf')
-        ]
 
         self.assertRaises(TypeError, pdf_combiner,
-                          test_list_of_paths_param, test_output_file_param)
+                          self.list_of_pdf_files, test_output_file_param)
 
     def tearDown(self) -> None:
         Path('merge.pdf').unlink(missing_ok=True)
