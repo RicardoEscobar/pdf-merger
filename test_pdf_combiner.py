74e16e827db8c73b6eacd29266e3142c3a11dc6c
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-21 13:38:08 -0600

    Add 2 new tests to test_pdf_combiner.py

--- test_pdf_combiner.py
+++ test_pdf_combiner.py
@@ -9,6 +9,7 @@ class TestPdfCombiner(unittest.TestCase):
 
     def setUp(self) -> None:
         self.list_of_pdf_files = list(Path('.').glob('**/*.pdf'))
+        self.test_output_file_param = Path('merge.pdf')
 
     def test_pdf_list_param_is_a_collection_of_paths(self):
         """Validates that a list or a tuple of paths(pathlib.Paths) are given to the pdf_combiner function."""
@@ -21,10 +22,23 @@ class TestPdfCombiner(unittest.TestCase):
 
     def test_output_file_param_is_a_path(self):
         """Validates that the output_file parameter is actually a pathlib.Path object."""
-        test_output_file_param = 'merge.pdf'
 
         self.assertRaises(TypeError, pdf_combiner,
-                          self.list_of_pdf_files, test_output_file_param)
+                          self.list_of_pdf_files, self.test_output_file_param.__str__())
+
+    def test_file_already_exists(self):
+        """Validate File already exists exception raised"""
+        pdf_combiner(self.list_of_pdf_files, self.test_output_file_param)
+        self.assertRaises(FileExistsError, pdf_combiner,
+                          self.list_of_pdf_files, self.test_output_file_param)
+
+    def test_overwrite_file(self):
+        """Validate file can be overwritten if overwrite_output_file parameter is set to True."""
+        pdf_combiner(self.list_of_pdf_files,
+                     self.test_output_file_param, allow_overwrite=True)
+
+        self.assertTrue(self.test_output_file_param.exists(),
+                        msg=f'{self.test_output_file_param} file doesn\'t exists.')
 
     def tearDown(self) -> None:
         Path('merge.pdf').unlink(missing_ok=True)
