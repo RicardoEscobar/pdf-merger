5b81c346f0495f730a3248023e932ff92ae0abb3
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-21 08:53:19 -0600

    Starting point for the main app.py

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
-from controller.pdf_combiner import pdf_combiner
-from controller.greet import greet
 
-root = tk.Tk()
-root.title("Greeter")
 
-# Here we create an instances of the StringVar() class, which is to track the content of widgets
-user_name = tk.StringVar()
+class HelloWorld(tk.Tk):
 
-# We define two frames to keep the input on different lines. In the next version we will switch to grid() geometry.
-# Padding accepts a tuple of up to four values. Clockwise like CSS.
-input_frame = ttk.Frame(root, padding=(20, 10, 20, 0))
-input_frame.pack(fill="both")
+    def __init__(self):
+        super().__init__()
 
-name_label = ttk.Label(input_frame, text="Name: ")
-name_label.pack(side="left", padx=(0, 10))
-name_entry = ttk.Entry(input_frame, width=15, textvariable=user_name)
-name_entry.pack(side="left")
-name_entry.focus()
+        self.title("Hello World!")
 
-buttons = ttk.Frame(root, padding=(20, 10))
-buttons.pack(fill="both")
+        ttk.Label(self, text="Hello, World!").pack()
 
-greet_button = ttk.Button(buttons, text="Greet",
-                          command=lambda: greet(user_name.get()))
-greet_button.pack(side="left", fill="x", expand=True)
-quit_button = ttk.Button(buttons, text="Quit", command=root.destroy)
-quit_button.pack(side="right", fill="x", expand=True)
 
+root = HelloWorld()
 root.mainloop()
