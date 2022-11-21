eef8e56b67272d1dd51e119f9ff2df075fba603b
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-21 10:32:47 -0600

    Add main_frame to app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from view.set_dpi_awareness import set_dpi_awareness
 from view.pdf_merger import PdfMerger
+from view.main_frame import MainFrame
+
 
 set_dpi_awareness()
 
-
 root = PdfMerger()
+main_frame = MainFrame(root).pack()
 root.mainloop()
