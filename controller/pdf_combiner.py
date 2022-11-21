f031f3c3eebe4f63042cf3a3659bc83506a0ad28
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-21 13:38:45 -0600

    Add allow_overwrite functionality to controller\pdf_combiner.py

--- controller/pdf_combiner.py
+++ controller/pdf_combiner.py
@@ -3,10 +3,15 @@ import PyPDF2
 from pathlib import Path
 
 
-def pdf_combiner(pdf_collection: List[Path], output_file: Path = Path('out.pdf')) -> None:
-    """pdf_combiner function gets a list or tuple of Paths (pathlib.Path) pointing at PDF files, then merges
-    them into a single PDF file of the name defined in output_file parameter or out.pdf if no
-    argument is passed to the output_file parameter.
+def pdf_combiner(pdf_collection: List[Path] | Tuple[Path],
+                 output_file: Path = Path('out.pdf'),
+                 allow_overwrite:  bool = False) -> None:
+    """pdf_combiner function gets a list or tuple of Paths (pathlib.Path) pointing at PDF files,
+    then merges them into a single PDF file of the name defined in output_file parameter or out.pdf
+    if no argument is passed to the output_file parameter.
+
+    Use allow_overwrite parameter (default=False) to set if you'd like to overwrite an existing file
+    during merge.
 
     Examples:
     pdf_filepaths = [Path('file1.pdf'), Path('file2.pdf'), Path('file3.pdf')]
@@ -14,6 +19,7 @@ def pdf_combiner(pdf_collection: List[Path], output_file: Path = Path('out.pdf')
     pdf_combiner(pdf_filepaths) # out.pdf is the merged PDF file.
     pdf_combiner(tuple(pdf_filepaths)) # Using a tuple of paths.
     pdf_combiner(pdf_filepaths, output_file=Path('merge.pdf'))
+    pdf_combiner(pdf_filepaths, output_file=Path('merge.pdf'), allow_overwrite=True)
     """
     if not isinstance(pdf_collection, (List, Tuple)):
         raise TypeError("Parameter pdf_list, is not of type List or Tuple")
@@ -26,7 +32,7 @@ def pdf_combiner(pdf_collection: List[Path], output_file: Path = Path('out.pdf')
     if not isinstance(output_file, Path):
         raise TypeError("Parameter output_file, is not of type Path")
 
-    if output_file.exists():
+    if output_file.exists() and not allow_overwrite:
         raise FileExistsError(
             f"Can't create the {output_file} output file. It already exists.")
 
