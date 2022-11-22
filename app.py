0dadcb89bc9912146211d3532a0b7bbe82b180ab
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-22 14:51:47 -0600

    Refactor unused assignation to a usable one app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ from view.main_frame import MainFrame
 set_dpi_awareness()
 
 root = PdfMerger()
-main_frame = MainFrame(root).pack()
+main_frame = MainFrame(root)
+main_frame.pack()
 root.mainloop()
