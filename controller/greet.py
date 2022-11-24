feb1b64141bf38d53e8fc5d30a3f877c1457aa84
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-24 15:23:55 -0600

    Add greet function the ability to return a str with the message instead of printig it to the consople controller\greet.py

--- controller/greet.py
+++ controller/greet.py
@@ -1,14 +1,19 @@
 """2022-11-20 09:08:57"""
 
 
-def greet(user_name: str = None) -> None:
+def greet(user_name: str = None) -> str:
     """This functions greets you on the console. Send an argument to user_name parameter to set the
     name of the person to greet, or leave it empty to greet the world."""
 
-    if not isinstance(user_name, str) and user_name is not None:
+    if user_name is None:
+        user_name = 'World!'
+    else:
+        user_name = ''.join([user_name, '.'])
+
+    if not isinstance(user_name, str):
         raise TypeError("Parameter user_name, is not of type str (string)")
 
-    print(f"Hello, {user_name or 'World!'}")
+    return f"Hello, {user_name}"
 
 
 if __name__ == '__main__':
