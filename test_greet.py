7a21f85d4fd56bb5fa561a9f616227bf5cac5daf
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-25 11:46:27 -0600

    Crates a logging system for test_greet.py

--- test_greet.py
+++ test_greet.py
@@ -3,9 +3,17 @@ import unittest
 import logging
 from controller.greet import greet
 
-# Configure the logging system to create a 'test_greet.log' file to test out the logging system.
-logging.basicConfig(filename='test_greet.log',
-                    level=logging.INFO, format='%(levelname)s:%(message)s')
+# Crates a logging system for this unit test file
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+
+file_handler_debug = logging.FileHandler('error.log')
+file_handler_debug.setLevel(logging.ERROR)
+file_handler_debug.setFormatter(formatter)
+
+logger.addHandler(file_handler_debug)
 
 
 class TestGreet(unittest.TestCase):
@@ -15,7 +23,7 @@ class TestGreet(unittest.TestCase):
         """Test user_name is str or None"""
         test_user_name_param = 1
         self.assertRaises(TypeError, greet, test_user_name_param)
-        logging.info('Test user_name is str or None')
+        logger.info('Test INFO user_name is str or None')
 
     def test_greet_returns_str(self):
         """Test that the greet function returns a str with the message."""
@@ -23,19 +31,19 @@ class TestGreet(unittest.TestCase):
         expected_result = "Hello, Jorge."
         actual_result = greet(user_name)
         self.assertEqual(expected_result, actual_result)
-        logging.info('greet("Jorge") = %s', actual_result)
+        logger.debug('greet("Jorge") = %s', actual_result)
 
     def test_greet_returns_hello_world(self):
         """Test that the greet function returns 'Hello, World!' message."""
         expected_result = "Hello, World!"
         actual_result = greet()
         self.assertEqual(expected_result, actual_result)
-        logging.info('greet() = %s', actual_result)
+        logger.debug('greet() = %s', actual_result)
 
         expected_result = "Hello, World!"
         actual_result = greet(None)
         self.assertEqual(expected_result, actual_result)
-        logging.info('greet(None) = %s', actual_result)
+        logger.debug('greet(None) = %s', actual_result)
 
 
 if __name__ == '__main__':
