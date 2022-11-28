79ab8061fc93c6a1e451347d689540f7817354b9
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-27 18:08:53 -0600

    Repacks the app insise a container frame app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from tkinter import ttk
 from view.set_dpi_awareness import set_dpi_awareness
 from view.pdf_merger import PdfMerger
 from view.main_frame import MainFrame
@@ -6,6 +7,8 @@ from view.main_frame import MainFrame
 set_dpi_awareness()
 
 root = PdfMerger()
-main_frame = MainFrame(root)
-main_frame.pack()
+container = ttk.Frame(root)
+main_frame = MainFrame(container)
+container.pack(fill='both', expand=True)
+main_frame.pack(fill='both', expand=True)
 root.mainloop()
