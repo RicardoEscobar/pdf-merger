e4fd679bbb9ce7e2b72c546d6760456048669fa2
Jorge Ricardo Escobar Carrasco <jorge.ricardo.escobar@gmail.com>
2022-11-27 18:10:11 -0600

    Add clear_list, clear_selected methods to MainFrame class in view\main_frame.py

--- view/main_frame.py
+++ view/main_frame.py
@@ -1,7 +1,9 @@
 """Main frame that shows the app to the user."""
+import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
 from pathlib import Path
+import logging
 from controller.pdf_combiner import pdf_combiner
 from controller.str_to_path import str_to_path
 
@@ -13,35 +15,75 @@ class MainFrame(ttk.Frame):
         """Init method modified to accomodate user_input."""
         super().__init__(container)
 
+        self.logger = logging.getLogger('test_main_frame.main_frame.MainFrame')
         self.source_filenames = tuple()
         self.merge_filename = Path('merge.pdf')
         self.filetypes = (
             ('PDF files', '*.pdf'),
             ('All files', '*.*')
         )
+        self.pdf_listvar = tk.StringVar()
 
-        label = ttk.Label(self, text="Select PDF files to merge: ")
-        button = ttk.Button(self, text="Open PDF file(s)",
-                            command=self.open_pdf_load_files_dialog)
+        button_open_pdf = ttk.Button(self, text="Open PDF file(s)",
+                                     command=self.open_pdf_load_files_dialog, width=20)
+        button_merge_pdf = ttk.Button(self, text="Merge PDF file",
+                                      command=self.open_pdf_merge_file_dialog, width=20)
+        button_clear_listbox = ttk.Button(self, text="Clear list",
+                                          command=self.clear_list, width=20)
+        button_clear_selected = ttk.Button(self, text="Clear selected",
+                                           command=self.clear_selected, width=20)
+        self.pdf_listbox = tk.Listbox(
+            self,
+            listvariable=self.pdf_listvar,
+            height=5,  # len(self.source_filenames),
+            selectmode='extended')
 
-        label.pack(side="left")
-        button.pack(side="left")
+        button_open_pdf.pack(side="top", ipady=5, ipadx=80)
+        button_merge_pdf.pack(side="top", ipady=5, ipadx=80)
+        button_clear_listbox.pack(side="top", ipady=5, ipadx=80)
+        button_clear_selected.pack(side="top", ipady=5, ipadx=80)
+        self.pdf_listbox.pack(side="top", fill='both', expand=True)
 
     def open_pdf_load_files_dialog(self) -> None:
-        self.source_filenames = fd.askopenfilenames(
+        self.source_filenames = self.source_filenames + fd.askopenfilenames(
             title='Load PDF files',
             filetypes=self.filetypes,
             defaultextension='.pdf')
 
-        if len(self.source_filenames) > 0:
-            self.open_pdf_merge_file_dialog()
+        # Updates listbox widget after opening file list
+        self.pdf_listvar.set(self.source_filenames)
+        self.pdf_listbox['height'] = len(self.source_filenames)
 
     def open_pdf_merge_file_dialog(self) -> None:
-        self.merge_filename = Path(fd.asksaveasfilename(
-            title='Save PDF merge as...',
-            filetypes=self.filetypes,
-            defaultextension='.pdf'))
+        if len(self.source_filenames) > 0:
+            self.merge_filename = Path(fd.asksaveasfilename(
+                title='Save PDF merge as...',
+                filetypes=self.filetypes,
+                defaultextension='.pdf'))
 
-        pdf_combiner(str_to_path(self.source_filenames),
-                     Path(self.merge_filename),
-                     allow_overwrite=True)
+            pdf_combiner(str_to_path(self.source_filenames),
+                         Path(self.merge_filename),
+                         allow_overwrite=True)
+        else:
+            raise ValueError('The list of PDF files is empty.')
+
+    def clear_list(self) -> None:
+        """Clear listbox of all PDF filenames."""
+        self.source_filenames = tuple()
+        self.pdf_listvar.set(self.source_filenames)
+
+    def clear_selected(self) -> None:
+        """Clear listbox of selected PDF filenames."""
+        self.logger.debug('Clear selected ran.')
+        # seleccionar items
+        self.logger.debug('pdf_listbox.curselection=%s',
+                          self.pdf_listbox.curselection())
+
+        for line in self.pdf_listbox.curselection():
+            self.pdf_listbox.delete(line)
+
+        end_index = self.pdf_listbox.size() - 1
+        new_tuple_pdf = self.pdf_listbox.get(0, end_index)
+
+        self.source_filenames = new_tuple_pdf
+        self.pdf_listvar.set(self.source_filenames)
